38fe3c6fa5857fc3e6de208cfdc680e00539169e
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-08-03 08:32:04 +0545

    typed errorboundary fetchsearch fetchpet and pet.tsx

--- src/fetchSearch.ts
+++ src/fetchSearch.ts
@@ -1,4 +1,16 @@
-async function fetchSearch({ queryKey }) {
+import { QueryFunction } from "@tanstack/react-query";
+import { PetAPIResponse, Animal } from "./APIResponseTypes";
+const fetchSearch: QueryFunction<
+  PetAPIResponse,
+  [
+    "search",
+    {
+      animal: Animal;
+      location: string;
+      breed: string;
+    }
+  ]
+> = async ({ queryKey }) => {
   const { animal, location, breed } = queryKey[1];
   const res = await fetch(
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
@@ -8,6 +20,6 @@ async function fetchSearch({ queryKey }) {
     throw new Error(`pet search not okay: ${animal}, ${location}, ${breed}`);
 
   return res.json();
-}
+};
 
 export default fetchSearch;
